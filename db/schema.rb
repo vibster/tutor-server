61c85d1f771e60dd3c30c8733a8bdd652dc2478c
Dante Soares <dante.m.soares@rice.edu>
2016-12-12 18:07:23 -0600

    Added number/title to admin catalog offerings view; Made ListOfferings respect the sort order; Fixed migration broken by the changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161108152717) do
-=======
 ActiveRecord::Schema.define(version: 20161212211910) do
->>>>>>> 3654555... Add title and order to catalog offerings
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
